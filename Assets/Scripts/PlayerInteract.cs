bd036f089c6678ebfb72bf326feccd518d87ca9d
Halak Pandya <120320032+Halakspandya@users.noreply.github.com>
2024-10-23 08:43:55 +0100

    Update PlayerInteract.cs

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// class for checking if the NPC is in range of the player and if key E is pressed
 public class PlayerInteract : MonoBehaviour
 {
     // private bool isInteracting = false;
@@ -10,11 +11,13 @@ public class PlayerInteract : MonoBehaviour
 
     private void Update (){
         if (Input.GetKeyDown(KeyCode.E)){
-            float interactRange =0.5f;
-            Collider [] colliderArray = Physics.OverlapSphere(transform.position,interactRange);
+            //radius for the imaginary sphere around the player to detect interactable objects with the player being the cnetre
+            float interactRange =0.5f;  
+            // function creates an imaginary sphere around a specified point in space (typically the player's position), with a defined radius.
+            Collider [] colliderArray = Physics.OverlapSphere(transform.position,interactRange); 
             foreach (Collider collider in colliderArray){
-                 var npcInteractable = collider.GetComponent<NPCInteractable>();
-                    npcInteractable.StartDialogue(); 
+                 var npcInteractable = collider.GetComponent<NPCInteractable>(); //getting the NPCInteractable component
+                    npcInteractable.StartDialogue(); //calling StartDialogue method from npcInteractable class
             }
         }
     }
