1875f67ef6ddbbe2f9154162e0194a322cbdf842
AtIlinov <atilinov94@gmail.com>
2024-12-02 14:16:29 +0000

    Key + Health pickup prefabs added, player interact has been seperated
    player interactions script is in a separate file

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -6,19 +6,47 @@ using UnityEngine.UI;
 // class for checking if the NPC is in range of the player and if key E is pressed
 public class PlayerInteract : MonoBehaviour
 {
-    // private bool isInteracting = false;
-    // [SerializeField] private GameObject interactUI;  // UI element for "Press E to interact"
 
-    private void Update (){
-        if (Input.GetKeyDown(KeyCode.E)){
-            //radius for the imaginary sphere around the player to detect interactable objects with the player being the cnetre
-            float interactRange =0.5f;  
-            // function creates an imaginary sphere around a specified point in space (typically the player's position), with a defined radius.
-            Collider [] colliderArray = Physics.OverlapSphere(transform.position,interactRange); 
-            foreach (Collider collider in colliderArray){
-                 var npcInteractable = collider.GetComponent<NPCInteractable>(); //getting the NPCInteractable component
-                    npcInteractable.StartDialogue(); //calling StartDialogue method from npcInteractable class
-            }
-        }
+    public Transform cam4ray;
+        //Variables to do with Door Interactions\\
+    public float MaxUseDistance = 1f;
+    public LayerMask UseLayers;
+    RaycastHit hit;
+    private GameObject playerObj;
+
+
+    void Start()
+    {
+
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+
     }
+
+    // Method to use the binding set up in the "Use" action in the input system
+    public void OnUse()
+    {
+        // Use Raycast to detect how far away the player's front is from an object
+        if (Physics.Raycast(cam4ray.position, cam4ray.forward, out hit, MaxUseDistance, UseLayers))
+        {
+            Debug.DrawRay(cam4ray.position, cam4ray.forward, Color.green);
+            // Get Door collider component and see if it's been hit
+            if(hit.collider.TryGetComponent<Door>(out Door door))
+            {
+                //if door is open, then run close method. Otherwise open door with open method
+                if (door.isOpen)
+                {
+                    door.Close();
+                }
+                else
+                {
+                    door.Open(playerObj.transform.position);
+                }
+                
+            }
+        } 
+    }
+
+
+
+
 }
