c485d96d0f7b47523142e87754657dc6595c6ed8
S-Parker03 <120114831+S-Parker03@users.noreply.github.com>
2025-01-01 18:11:11 +0000

    Inventory now works with picking up items
    Fixed rendering for picked up items

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 //this scriptable object is used to create items that can be stored in the inventory
 //made with help from this tutorial: https://gamedevbeginner.com/how-to-make-an-inventory-system-in-unity/#inventory_systems
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+
 [System.Serializable]
 public class ItemInfo : MonoBehaviour
 {
