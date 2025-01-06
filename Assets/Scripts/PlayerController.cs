332c5443e6bb84a08a070a67298939f78f266c68
AtIlinov <atilinov94@gmail.com>
2025-01-06 09:03:36 +0000

    Door error fixes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -103,30 +103,6 @@ public class PlayerController : MonoBehaviour
         // playerCollider = GameObject.FindGameObjectWithTag("GroundCollider");
     }
 
-    // // Method to use the binding set up in the "Use" action in the input system
-    // public void OnUse()
-    // {
-    //     // Use Raycast to detect how far away the player's front is from an object
-    //     if (Physics.Raycast(cam4ray.position, cam4ray.forward, out RaycastHit hit, MaxUseDistance, UseLayers))
-    //     {
-    //         Debug.DrawRay(cam4ray.position, cam4ray.forward, Color.green);
-    //         // Get Door collider component and see if it's been hit
-    //         if(hit.collider.TryGetComponent<Door>(out Door door))
-    //         {
-    //             //if door is open, then run close method. Otherwise open door with open method
-    //             if (door.isOpen)
-    //             {
-    //                 door.Close();
-    //             }
-    //             else
-    //             {
-    //                 door.Open(transform.position);
-    //             }
-                
-    //         }
-    //     } 
-    // }
-
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
         if(isGrounded && moveValue.magnitude > 0.1f && !footsteps.isPlaying){
